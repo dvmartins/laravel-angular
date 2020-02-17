3de8bc5c04cad10a79cc9796a05ab8f0730b9185
dvmartins <dvmartins_@outlook.com>
2020-02-17 13:59:08 -0300

    First Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+        <title>Laravel</title>
+       
+    </head>
+    <body>
+
+        <app-root></app-root>
+
+        <script type="text/javascript" src="{{ asset('js/runtime-es5.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/polyfills-es5.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/styles-es5.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/vendor-es5.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/main-es5.js') }}"></script>
+
+    </body>
+</html>
